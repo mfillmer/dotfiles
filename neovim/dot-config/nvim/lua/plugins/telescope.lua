a40d041417ae2be48f06a3342cf1ad2b9b1056df
malte <malte.fillmer@gmail.com>
2024-12-21 21:08:21 +0100

    configure basics of nvim

---
+++ neovim/dot-config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-p', builtin.find_files, {})
+return {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
