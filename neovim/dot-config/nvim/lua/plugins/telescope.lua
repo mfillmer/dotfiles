f97ccb223fe4860d64c87a617096d8ecb87c5c1e
malte <malte.fillmer@gmail.com>
2024-12-21 22:02:39 +0100

    update telescope

--- neovim/dot-config/nvim/lua/plugins/telescope.lua
+++ neovim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,24 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<C-p', builtin.find_files, {})
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<C-F>", builtin.live_grep, {})
+
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
