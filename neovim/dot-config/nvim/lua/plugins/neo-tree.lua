2dc293eaea7974f1c81fba7baa201c47c0ab036c
malte <malte.fillmer@gmail.com>
2024-12-31 18:03:42 +0100

    change keybinding from reveal to toggle

--- neovim/dot-config/nvim/lua/plugins/neo-tree.lua
+++ neovim/dot-config/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }, 
     config = function()
-		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
 		vim.keymap.set("n", "<C-b>", ":Neotree buffers reveal float<CR>", {})
     end
 }
